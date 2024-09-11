22ac4042b55923f2bd821d6652154e2153d32426
dgentzel1 <dgentzel1@gmail.com>
2024-09-10 22:18:13 -0400

    Assignment 1a David Gentzel

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "dgentzel1";
+    char STUDENT_ID[] = "964083870";
 
     return 0 ;
 }
